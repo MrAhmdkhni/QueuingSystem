606741c50c584692dc731f7b43c5860c510096fc
Amirhosin <popularamirhosein@gmail.com>
2023-02-01 18:34:09 +0330

    add viewAllVisitor and viewAllCustomer method

--- src/main/java/ir/rabin_andisheh/setmeetingtime/controller/AdminController.java
+++ src/main/java/ir/rabin_andisheh/setmeetingtime/controller/AdminController.java
@@ -1,24 +1,27 @@
 package ir.rabin_andisheh.setmeetingtime.controller;
 
+import ir.rabin_andisheh.setmeetingtime.dto.PersonsDTO;
 import ir.rabin_andisheh.setmeetingtime.dto.VisitorQueuingDTO;
 import ir.rabin_andisheh.setmeetingtime.entity.Queuing;
 import ir.rabin_andisheh.setmeetingtime.service.AdminService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.modelmapper.ModelMapper;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/admin")
 public class AdminController {
 
     private final AdminService adminService;
+    private final ModelMapper mapper;
 
-    public AdminController(AdminService adminService) {
+    public AdminController(AdminService adminService, ModelMapper mapper) {
         this.adminService = adminService;
+        this.mapper = mapper;
     }
 
     @PostMapping("/visitorAppointmentTime")
@@ -32,5 +35,23 @@ public class AdminController {
                         .build(),
                 visitorQueuingDTO.getDurationTime()
         );
+    }
+
+    @GetMapping("/viewAllVisitor")
+    public List<PersonsDTO> viewAllVisitor() {
+        List<PersonsDTO> personsDTOS = new ArrayList<>();
+        adminService.viewAllVisitor().forEach(
+                visitor -> personsDTOS.add(mapper.map(visitor, PersonsDTO.class))
+        );
+        return personsDTOS;
+    }
+
+    @GetMapping("/viewAllCustomer")
+    public List<PersonsDTO> viewAllCustomer() {
+        List<PersonsDTO> personsDTOS = new ArrayList<>();
+        adminService.viewAllCustomer().forEach(
+                visitor -> personsDTOS.add(mapper.map(visitor, PersonsDTO.class))
+        );
+        return personsDTOS;
     }
 }
