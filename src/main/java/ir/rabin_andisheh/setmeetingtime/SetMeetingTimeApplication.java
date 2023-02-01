10e03adc9d11df2fb625306e816c797fda24cc3d
Amirhosin <popularamirhosein@gmail.com>
2023-02-01 17:44:42 +0330

    complete SetMeetingTimeApplication class

---
+++ src/main/java/ir/rabin_andisheh/setmeetingtime/SetMeetingTimeApplication.java
@@ -0,0 +1,26 @@
+package ir.rabin_andisheh.setmeetingtime;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@SpringBootApplication
+public class SetMeetingTimeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SetMeetingTimeApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder bCryptPasswordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        return new ModelMapper();
+    }
+
+}
