17791eebda6e01f90ac09ab357836fe06014d81d
Amirhosin <popularamirhosein@gmail.com>
2023-02-01 18:34:33 +0330

    add CustomerNotFoundException class

---
+++ src/main/java/ir/rabin_andisheh/setmeetingtime/exception/CustomerNotFoundException.java
@@ -0,0 +1,8 @@
+package ir.rabin_andisheh.setmeetingtime.exception;
+
+public class CustomerNotFoundException extends RuntimeException {
+
+    public CustomerNotFoundException(String message) {
+        super(message);
+    }
+}
